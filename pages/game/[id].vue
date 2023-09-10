292eb806b090a7828a092bb62eca068d9475fd7f
yichienlu <yichien.lu89@gmail.com>
2023-09-10 23:20:42 +0800

    feat(game): section2 layout (YC)

--- pages/game/[id].vue
+++ pages/game/[id].vue
@@ -6,31 +6,32 @@
 <section class="px-3 bg-section text-light">
   <div class="container mx-auto">
 
-    <div class="grid grid-cols-3 gap-4">
-      <div class="col-span-2">
+    <div class="grid md:grid-cols-3 gap-4">
+      <div class="md:col-span-2">
         <div id="description">
-          <div class="border border-labellight px-11 py-8">
-            <h2>
-              <span>遊戲簡介</span>
-              <span>description</span>
+          <div class="border border-labellight px-11 py-8 rounded-[20px]">
+            <h2 class="mb-5">
+              <span class="text-28 mr-3">遊戲簡介</span>
+              <span class="text-24 font-medium">description</span>
             </h2>
-            <ul id="tags" class="flex">
-              <li>tag1</li>
-              <li>tag1</li>
-              <li>tag1</li>
-              <li>tag1</li>
-              <li>tag1</li>
+            <ul id="tags" class="flex text-[12px] text-white">
+              <li class="px-3 py-1 rounded bg-tab mr-2">合作</li>
+              <li class="px-3 py-1 rounded bg-tab mr-2">多人</li>
+              <li class="px-3 py-1 rounded bg-tab mr-2">解謎</li>
+              <li class="px-3 py-1 rounded bg-tab mr-2">分割畫面</li>
+              <li class="px-3 py-1 rounded bg-tab mr-2">單機合作</li>
             </ul>
-
-
-          </div>
-          <ul>
+ <ul>
             <li>發行日期：2021/03/26 </li>
             <li>開發人員：Hazelight</li>
             <li>發行商：Electronic Arts</li>
           </ul>
+            <div class="bg-tab w-16 h-16 flex justify-center items-center">9.2</div>
+            <button class="py-4 text-secondary border border-secondary rounded-lg md:rounded-xl w-full md:w-64">評論此遊戲</button>
+          </div>
+         
 
-          <div class="bg-tab w-16 h-16 flex justify-center items-center">9.2</div>
+          
 
           <p>
             在《雙人成行》這款專為雙人合作遊玩打造，跨越類型界線的橫向捲軸冒險遊戲中，踏上人生中最瘋狂的旅程。利用好友通行證*邀請好友免費遊玩。遊玩一對經常起爭執的夫妻科迪與小梅，兩人因為魔法而變成了玩偶。這兩個人被困在一個充滿驚喜的奇幻世界中，並且不得不挽救他們之間的感情。在每個新關卡中掌握獨特且有關連的角色技能。幫助彼此跨越各種令人意外的障礙與爆笑的時刻。踹向流氓松鼠毛茸茸的尾巴、駕駛一條內褲、在擁擠的夜店中成為 DJ，並在魔法雪景球中搭乘雪橇。體驗一段溫馨且爆笑  ...  
@@ -40,9 +41,9 @@
           </p>
         </div>
         <div id="special" class="md:hidden">
-          <h2>
-            <span>超值組合包</span>
-            <span>Special</span>
+          <h2 class="pb-1 border-b border-secondary md:border-0 mb-5 md:mb-6">
+            <span class="text-28 mr-3">超值組合包</span>
+            <span class="text-24 font-medium">Special</span>
           </h2>
           <ul>
             <li class="border border-labellight px-4 pt-6 pb-5">
@@ -63,17 +64,24 @@
         </div>
 
         <div id="news">
-          <h2>
-            <span>新聞</span>
-            <span>News</span>
+          <h2 class="pb-1 border-b border-secondary  mb-5 md:mb-6 md:w-1/2 relative 
+          after:absolute 
+          after:w-full 
+          after:top-full 
+          after:right-full 
+          after:h-[1px] 
+          after:bg-secondary">
+            <span class="text-28 mr-3">新聞</span>
+            <span class="text-24 font-medium">News</span>
           </h2>
           <ul>
-            <li>
-              <div class="flex">
-                <div>圖</div>
-                <div>
-                  <h3>A CRAZY JOURNEY - 10 MILLION UNITS SOLD!</h3>
-                  <p>It’s so amazing to see all the support for It Takes Two. Thank you to all the fans and community for going on this magical journey together with us!-Hazelight Studios</p>
+            <li class="mb-10">
+              <div class="md:flex items-center">
+                <div class="h-[152px] md:h-[200px] md:w-1/2 md:mr-14 bg-red-400">圖</div>
+                <!-- <img src="" class="h-[152px] md:h-[200px]" alt=""> -->
+                <div class=" w-1/2">
+                  <h3 class="text-20 mb-2">A CRAZY JOURNEY - 10 MILLION UNITS SOLD!</h3>
+                  <p class="">It’s so amazing to see all the support for It Takes Two. Thank you to all the fans and community for going on this magical journey together with us!-Hazelight Studios</p>
                 </div>
               </div>
 
@@ -118,3 +126,13 @@
 </section>
 
 </template>
+
+<style scoped>
+
+.multiline-ellipsis { 
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 5;
+  overflow: hidden;
+}
+</style>
