741db7a2510f063bcc784bbb236c352e1c04e78c
twhg12f <twhg12f@gmail.com>
2023-09-13 15:48:38 +0800

    feat(game):section2 laout (YC)

--- pages/game/[id].vue
+++ pages/game/[id].vue
@@ -3,123 +3,165 @@
 <SectiononeComponent></SectiononeComponent>
 
 <!-- section 2 -->
-<section class="px-3 bg-section text-light">
+<section class="px-3 py-10 lg:py-14 bg-section text-light">
   <div class="container mx-auto">
-
-    <div class="grid md:grid-cols-3 gap-4">
-      <div class="md:col-span-2">
-        <div id="description">
-          <div class="border border-labellight px-11 py-8 rounded-[20px]">
-            <h2 class="mb-5">
-              <span class="text-28 mr-3">遊戲簡介</span>
-              <span class="text-24 font-medium">description</span>
-            </h2>
-            <ul id="tags" class="flex text-[12px] text-white">
-              <li class="px-3 py-1 rounded bg-tab mr-2">合作</li>
-              <li class="px-3 py-1 rounded bg-tab mr-2">多人</li>
-              <li class="px-3 py-1 rounded bg-tab mr-2">解謎</li>
-              <li class="px-3 py-1 rounded bg-tab mr-2">分割畫面</li>
-              <li class="px-3 py-1 rounded bg-tab mr-2">單機合作</li>
-            </ul>
- <ul>
-            <li>發行日期：2021/03/26 </li>
-            <li>開發人員：Hazelight</li>
-            <li>發行商：Electronic Arts</li>
-          </ul>
-            <div class="bg-tab w-16 h-16 flex justify-center items-center">9.2</div>
-            <button class="py-4 text-secondary border border-secondary rounded-lg md:rounded-xl w-full md:w-64">評論此遊戲</button>
+    <div class="grid gap-14 lg:grid-cols-3">
+      <div class="lg:col-span-2">
+        <div id="description" class="mb-10">
+          <div class="border border-labellight px-11 py-8 rounded-[20px] flex flex-wrap justify-between items-center gap-y-8 mb-5 lg:mb-10">
+            <div class="order-1 w-full lg:w-5/6">
+              <h2 class="mb-5">
+                <span class="mr-3 text-28">遊戲簡介</span>
+                <span class="font-medium text-24">description</span>
+              </h2>
+              <ul id="tags" class="flex text-[12px] text-white">
+                <li class="px-3 py-1 mr-2 rounded bg-tab">合作</li>
+                <li class="px-3 py-1 mr-2 rounded bg-tab">多人</li>
+                <li class="px-3 py-1 mr-2 rounded bg-tab">解謎</li>
+                <li class="px-3 py-1 mr-2 rounded bg-tab">分割畫面</li>
+                <li class="px-3 py-1 mr-2 rounded bg-tab">單機合作</li>
+              </ul>
+            </div>
+            <div class="flex items-center justify-center order-3 w-16 h-16 bg-tab lg:order-2 rounded-xl text-28 lg:text-40 lg:rounded-3xl lg:w-20 lg:h-20">9.2</div>
+              <ul class="order-2 lg:order-3 ">
+                <li>發行日期：2021/03/26 </li>
+                <li>開發人員：Hazelight</li>
+                <li>發行商：Electronic Arts</li>
+              </ul>
+              <button class="order-4 w-full py-3 border rounded-lg lg:w-64 text-secondary border-secondary md:rounded-xl text-20">評論此遊戲</button>
+            </div>
+            <p>
+              在《雙人成行》這款專為雙人合作遊玩打造，跨越類型界線的橫向捲軸冒險遊戲中，踏上人生中最瘋狂的旅程。利用好友通行證*邀請好友免費遊玩。遊玩一對經常起爭執的夫妻科迪與小梅，兩人因為魔法而變成了玩偶。這兩個人被困在一個充滿驚喜的奇幻世界中，並且不得不挽救他們之間的感情。在每個新關卡中掌握獨特且有關連的角色技能。幫助彼此跨越各種令人意外的障礙與爆笑的時刻。踹向流氓松鼠毛茸茸的尾巴、駕駛一條內褲、在擁擠的夜店中成為 DJ，並在魔法雪景球中搭乘雪橇。體驗一段溫馨且爆笑  ...  
+              <a href="#">閱讀更多</a>
+            </p>
           </div>
-         
+          <div id="combo" class="lg:hidden">
+            <h2 class="pb-1 mb-5 border-b border-secondary relative 
+            after:absolute 
+            after:w-full 
+            after:top-full 
+            after:right-full 
+            after:h-[1px] 
+            after:bg-secondary">
+              <span class="mr-3 text-28">超值組合包</span>
+              <span class="font-medium text-24">Special</span>
+            </h2>
+            <ul>
+              <li class="px-4 pt-6 pb-5 mb-7 border border-labellight  rounded-[20px]">
+                <h3 class="mb-2 text-24">購買豪華禮包</h3>
+                <p class="mb-4 text-18">包含PUNG:BATTLEGROUNDS 遊戲本體 + DLC</p>
+                <div class="flex items-center mb-6">
+                  <div class="[text-shadow:_16px_12px_0_rgb(255_255_255_/_5%),_0_5px_5px_#0004] text-32 font-bold text-labeldark">$1982</div>
+                  <div class="ml-6">
+                    <div class="mb-0 font-bold leading-5 text-20 text-labellight">-42%</div>
+                    <div class="leading-3 line-through text-12">1982</div>
+                  </div>
+                </div>
+                <button class="w-full py-3 border rounded-lg border-secondary text-secondary">加入購物車</button>
+              </li>
+              <li class="px-4 pt-6 pb-5 mb-7 border border-labellight  rounded-[20px]">
+                <h3 class="mb-2 text-24">購買豪華禮包</h3>
+                <p class="mb-4 text-18">包含PUNG:BATTLEGROUNDS 遊戲本體 + DLC</p>
+                <div class="flex items-center mb-6">
+                  <div class="[text-shadow:_16px_12px_0_rgb(255_255_255_/_5%),_0_5px_5px_#0004] text-32 font-bold text-labeldark">$1982</div>
+                  <div class="ml-6">
+                    <div class="mb-0 font-bold leading-5 text-20 text-labellight">-42%</div>
+                    <div class="leading-3 line-through text-12">1982</div>
+                  </div>
+                </div>
+                <button class="w-full py-3 border rounded-lg border-secondary text-secondary">加入購物車</button>
+              </li>
 
-          
+            </ul>
+          </div>
 
-          <p>
-            在《雙人成行》這款專為雙人合作遊玩打造，跨越類型界線的橫向捲軸冒險遊戲中，踏上人生中最瘋狂的旅程。利用好友通行證*邀請好友免費遊玩。遊玩一對經常起爭執的夫妻科迪與小梅，兩人因為魔法而變成了玩偶。這兩個人被困在一個充滿驚喜的奇幻世界中，並且不得不挽救他們之間的感情。在每個新關卡中掌握獨特且有關連的角色技能。幫助彼此跨越各種令人意外的障礙與爆笑的時刻。踹向流氓松鼠毛茸茸的尾巴、駕駛一條內褲、在擁擠的夜店中成為 DJ，並在魔法雪景球中搭乘雪橇。體驗一段溫馨且爆笑  ...  
+          <div id="news">
+            <h2 class="pb-1 border-b border-secondary  mb-5 md:mb-6 md:w-1/2 relative 
+            after:absolute 
+            after:w-full 
+            after:top-full 
+            after:right-full 
+            after:h-[1px] 
+            after:bg-secondary">
+              <span class="mr-3 text-28">新聞</span>
+              <span class="font-medium text-24">News</span>
+            </h2>
+            <ul>
+              <li class="mb-10">
+                <a href="#" class="items-center md:flex">
+                  <div class="h-[152px] md:h-[200px] md:w-1/2 md:mr-14 bg-red-400">圖</div>
+                  <!-- <img src="" class="h-[152px] md:h-[200px]" alt=""> -->
+                  <div class="md:w-1/2 ">
+                    <h3 class="mt-8 mb-2 text-20 md:mt-0">A CRAZY JOURNEY - 10 MILLION UNITS SOLD!</h3>
+                    <p class="">It’s so amazing to see all the support for It Takes Two. Thank you to all the fans and community for going on this magical journey together with us!-Hazelight Studios</p>
+                  </div>
+                </a>
 
+              </li>
+              <li class="mb-10">
+                <a href="#" class="items-center md:flex">
+                  <div class="h-[152px] md:h-[200px] md:w-1/2 md:mr-14 bg-red-400">圖</div>
+                  <!-- <img src="" class="h-[152px] md:h-[200px]" alt=""> -->
+                  <div class="md:w-1/2 ">
+                    <h3 class="mt-8 mb-2 text-20 md:mt-0">A CRAZY JOURNEY - 10 MILLION UNITS SOLD!</h3>
+                    <p class="">It’s so amazing to see all the support for It Takes Two. Thank you to all the fans and community for going on this magical journey together with us!-Hazelight Studios</p>
+                  </div>
+                </a>
 
-            <a href="#">閱讀更多</a>
-          </p>
+              </li>
+            </ul>
+            <button class="block py-3 mx-auto border rounded-lg w-52 border-light text-light">顯示更多</button>
+          </div>
         </div>
-        <div id="special" class="md:hidden">
-          <h2 class="pb-1 border-b border-secondary md:border-0 mb-5 md:mb-6">
-            <span class="text-28 mr-3">超值組合包</span>
-            <span class="text-24 font-medium">Special</span>
+        
+
+        <!-- 右欄 -->
+        <div  class="hidden lg:block">
+          <h2 class="relative pb-1 mb-5 md:mb-6 ">
+            <span class="mr-3 text-28">超值組合包</span>
+            <span class="font-medium text-24">Special</span>
           </h2>
           <ul>
-            <li class="border border-labellight px-4 pt-6 pb-5">
-              <h3 class="text-24">購買豪華禮包</h3>
-              <p class="text-18">包含PUNG:BATTLEGROUNDS 遊戲本體 + DLC</p>
-              <div class="flex align-middle">
-                <div class="[text-shadow:_16px_12px_0_rgb(255_255_255_/_5%)] text-32 font-bold">$1982</div>
+            <li class="px-4 pt-6 pb-5 mb-7 border border-labellight  rounded-[20px]">
+              <h3 class="mb-2 text-24">購買豪華禮包</h3>
+              <p class="mb-4 text-18">包含PUNG:BATTLEGROUNDS 遊戲本體 + DLC</p>
+              <div class="flex items-center mb-6">
+                <div class="[text-shadow:_16px_12px_0_rgb(255_255_255_/_5%),_0_5px_5px_#0004] text-32 font-bold text-labeldark">$1982</div>
                 <div class="ml-6">
-                  <div class="text-20">-42%</div>
-                  <div class="line-through text-12">1982</div>
+                  <div class="mb-0 font-bold leading-5 text-20 text-labellight">-42%</div>
+                  <div class="leading-3 line-through text-12">1982</div>
                 </div>
               </div>
-              <button class="border border-secondary text-secondary">加入購物車</button>
+              <button class="w-full py-3 border rounded-lg border-secondary text-secondary">加入購物車</button>
             </li>
-            
-            
-          </ul>
-        </div>
-
-        <div id="news">
-          <h2 class="pb-1 border-b border-secondary  mb-5 md:mb-6 md:w-1/2 relative 
-          after:absolute 
-          after:w-full 
-          after:top-full 
-          after:right-full 
-          after:h-[1px] 
-          after:bg-secondary">
-            <span class="text-28 mr-3">新聞</span>
-            <span class="text-24 font-medium">News</span>
-          </h2>
-          <ul>
-            <li class="mb-10">
-              <div class="md:flex items-center">
-                <div class="h-[152px] md:h-[200px] md:w-1/2 md:mr-14 bg-red-400">圖</div>
-                <!-- <img src="" class="h-[152px] md:h-[200px]" alt=""> -->
-                <div class=" w-1/2">
-                  <h3 class="text-20 mb-2">A CRAZY JOURNEY - 10 MILLION UNITS SOLD!</h3>
-                  <p class="">It’s so amazing to see all the support for It Takes Two. Thank you to all the fans and community for going on this magical journey together with us!-Hazelight Studios</p>
-                </div>
-              </div>
-
-            </li>
-          </ul>
-          <button class="border border-light text-light">顯示更多</button>
-        </div>
-      </div>
-      <div>
-      <!-- slot for special -->
-      <div id="special" class="hidden md:block">
-          <h2>
-            <span>超值組合包</span>
-            <span>Special</span>
-          </h2>
-          <ul>
-            <li class="border border-labellight px-4 pt-6 pb-5">
-              <h3 class="text-24">購買豪華禮包</h3>
-              <p class="text-18">包含PUNG:BATTLEGROUNDS 遊戲本體 + DLC</p>
-              <div class="flex align-middle">
-                <div class="[text-shadow:_16px_12px_0_rgb(255_255_255_/_5%)] text-32 font-bold">$1982</div>
+            <li class="px-4 pt-6 pb-5 mb-7 border border-labellight  rounded-[20px]">
+              <h3 class="mb-2 text-24">購買豪華禮包</h3>
+              <p class="mb-4 text-18">包含PUNG:BATTLEGROUNDS 遊戲本體 + DLC</p>
+              <div class="flex items-center mb-6">
+                <div class="[text-shadow:_16px_12px_0_rgb(255_255_255_/_5%),_0_5px_5px_#0004] text-32 font-bold text-labeldark">$1982</div>
                 <div class="ml-6">
-                  <div class="text-20">-42%</div>
-                  <div class="line-through text-12">1982</div>
+                  <div class="mb-0 font-bold leading-5 text-20 text-labellight">-42%</div>
+                  <div class="leading-3 line-through text-12">1982</div>
                 </div>
               </div>
-              <button class="border border-secondary text-secondary">加入購物車</button>
+              <button class="w-full py-3 border rounded-lg border-secondary text-secondary">加入購物車</button>
             </li>
-            
-            
+            <li class="px-4 pt-6 pb-5 mb-7 border border-labellight  rounded-[20px]">
+              <h3 class="mb-2 text-24">購買豪華禮包</h3>
+              <p class="mb-4 text-18">包含PUNG:BATTLEGROUNDS 遊戲本體 + DLC</p>
+              <div class="flex items-center mb-6">
+                <div class="[text-shadow:_16px_12px_0_rgb(255_255_255_/_5%),_0_5px_5px_#0004] text-32 font-bold text-labeldark">$1982</div>
+                <div class="ml-6">
+                  <div class="mb-0 font-bold leading-5 text-20 text-labellight">-42%</div>
+                  <div class="leading-3 line-through text-12">1982</div>
+                </div>
+              </div>
+              <button class="w-full py-3 border rounded-lg border-secondary text-secondary">加入購物車</button>
+            </li>
           </ul>
         </div>
-      </div>
 
     </div>
-
-
-  
 
   </div>
   
