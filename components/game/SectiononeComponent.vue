1c0ccd0dd60a259daffe2a21d282763d389be179
su <anny81327@gmail.com>
2023-09-19 23:25:20 +0800

    fix : 調整、修正RWD

--- components/game/SectiononeComponent.vue
+++ components/game/SectiononeComponent.vue
@@ -1,43 +1,76 @@
 <template>
-  <div class="bg-sec-banner bg-cover">
-    <div class="container py-8 md:py-20">
-      <hr class="mb-2 border-secondary" />
+  <div class="bg-sec-banner bg-cover overflow-hidden">
+    <div class="container py-8 md:py-12 lg:py-16 xl:py-18 relative">
+      <hr
+        class="mb-2 border-secondary md:absolute -right-1/2 md:w-full top-[100px] "
+      />
       <h1 class="text-20 xl:text-40 font-bold mb-4 text-white">
         PUBG: BATTLEGROUNDS
       </h1>
       <!-- 中間區塊 -->
-      <div class="md:grid grid-cols-12 gap-14">
+      <div class="md:grid grid-cols-12 md:gap-5 xl:gap-14">
         <!-- 左 手機隱藏 -->
         <div class="md:col-span-8 md:block hidden">
-          <img src="/images/sec1-banner.jpg" alt="" />
+          <div class="lg:flex lg:flex-col lg:justify-between lg:h-full">
+            <img
+              src="/images/sec1-banner.jpg"
+              class="lg:h-full object-cover 2xl:h-auto"
+              alt=""
+            />
 
-          <div class="grid grid-cols-12 mt-10">
-            <img class="col-span-3" src="/images/sec1-img4.jpg" alt="" />
-            <img class="col-span-3" src="/images/sec1-img3.jpg" alt="" />
-            <img class="col-span-3" src="/images/sec1-img2.jpg" alt="" />
-            <img class="col-span-3" src="/images/sec1-img1.jpg" alt="" />
+            <div class="grid grid-cols-12 md:mt-3 lg:mt-22">
+              <img
+                class="md:col-span-6 lg:col-span-3"
+                src="/images/sec1-img4.jpg"
+                alt=""
+              />
+              <img
+                class="md:col-span-6 lg:col-span-3"
+                src="/images/sec1-img3.jpg"
+                alt=""
+              />
+              <img
+                class="md:col-span-6 lg:col-span-3 md:mt-3 lg:mt-0"
+                src="/images/sec1-img2.jpg"
+                alt=""
+              />
+              <img
+                class="md:col-span-6 lg:col-span-3 md:mt-3 lg:mt-0"
+                src="/images/sec1-img1.jpg"
+                alt=""
+              />
+            </div>
           </div>
         </div>
 
         <!-- 右 -->
-        <div class="md:col-span-4 h-full">
+        <div class="col-span-4 h-full">
           <div class="flex flex-col h-full">
             <!-- tittle -->
-            <div class="flex-1 mb-2">
-              <img src="/images/sec1-img5.jpg" alt="" />
-              <hr class="mb-3 mt-6 border-secondary" />
-              <p class="font-bold text-28 hidden xl:block">
+            <div class="flex-1 mb-2 relative ">
+              <img src="/images/sec1-img5.jpg" alt="" class="w-full mb-6" />
+              <p class="font-bold text-28 hidden xl:block mt-3">
                 PUBG: BATTLEGROUNDS
               </p>
-              <p>
+              <!-- 定位線 -->
+              <hr class="border-secondary md:absolute -right-1/2 md:w-full" />
+              <p class=" ">
                 在《雙人成行》中踏上人生中最瘋狂的旅程，利用好友通行證*邀請好友免費遊玩。
               </p>
             </div>
             <!-- price -->
-            <div class="flex justify-between items-center mb-7">
-              <div class="flex items-center font-medium relative">
+            <div
+              class="flex md:block lg:flex justify-between items-center mb-7 md:mb-4"
+            >
+              <div
+                class="flex items-center font-medium relative md:justify-between"
+              >
                 <p class="text-36 text-labeldark mr-6">$ 1,982</p>
-                <p class="text-36 opacity-5  mr-4 absolute translate-x-6 translate-y-4">$ 1,982</p>
+                <p
+                  class="text-36 opacity-5 mr-4 absolute translate-x-6 translate-y-4"
+                >
+                  $ 1,982
+                </p>
                 <div>
                   <p class="text-labellight leading-none text-32">- 42%</p>
                   <div class="relative text-center">
@@ -50,14 +83,14 @@
                 </div>
               </div>
 
-              <div class="flex">
+              <div class="flex md:mt-2 md:justify-end">
                 <Icon name="mdi:microsoft-windows" class="mr-1 text-24" />
                 <Icon name="mdi:linux" class="mr-1 text-24" />
                 <Icon name="mdi:laptop-mac" class="text-24" />
               </div>
             </div>
             <button
-              class="w-full button-sm md:button-xl button-hover mb-3 md:mb-5 flex"
+              class="w-full button-sm xl:button-xl button-hover mb-3 md:mb-5 flex"
             >
               <p class="w-10 ml-6 absolute opacity-40">BUY</p>
               <p class="flex-1">立即購買</p>
@@ -65,13 +98,13 @@
               <p></p>
             </button>
             <button
-              class="w-full button-outline-sm button-outline-hover md:button-outline-xl mb-3 md:mb-5"
+              class="w-full button-outline-sm button-outline-hover xl:button-outline-xl mb-3 md:mb-5"
             >
               <p class="w-10 ml-6 absolute opacity-40">Cart</p>
               <p class="flex-1">加入購物車</p>
             </button>
             <button
-              class="w-full button-outline-sm button-outline-hover md:button-outline-xl"
+              class="w-full button-outline-sm button-outline-hover xl:button-outline-xl"
             >
               <p class="w-10 ml-6 absolute opacity-40">Add</p>
               <p class="flex-1">加入願望清單</p>
