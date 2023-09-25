69fbebbe765fb341a52b1d7e0b125a0f9ac2d1d6
yichienlu <yichien.lu89@gmail.com>
2023-09-25 17:36:03 +0800

    feat: menu toggle

--- components/NavbarYC.vue
+++ components/NavbarYC.vue
@@ -1,16 +1,18 @@
 <script setup>
-// let menuOpen = ref(false)
-// const toggleMenu = () => {
-//   menuOpen.value = !menuOpen.value
-// }
+let menuOpen = ref(false)
+const toggleMenu = () => {
+  menuOpen.value = !menuOpen.value
+}
 
 
 </script>
 
 <template>
+  <div class="fixed top-0 left-0 right-0 w-full h-full backdrop-blur-lg z-20" :class="{'hidden':!menuOpen}"></div>
   <div id="navbar" class="relative ">
-    <div class="sticky z-20 w-full t-0 bg-darkgray bg-opacity-80">
-      <button type="button" class="absolute top-[50%] -translate-y-[50%] xl:hidden p-3" >
+    <div class="sticky z-20 w-full t-0 bg-primary bg-opacity-80">
+      <!-- MENU 鍵 -->
+      <button type="button" class="absolute top-[50%] -translate-y-[50%] xl:hidden p-3" @click="toggleMenu">
         <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M0 11.9996H18V9.99963H0V11.9996ZM0 6.99963H18V4.99963H0V6.99963ZM0 -0.000366211V1.99963H18V-0.000366211H0Z" fill="white"/>
         </svg>
@@ -19,7 +21,7 @@
         <NuxtLink to="/" class="w-auto mx-auto xl:mr-8 xl:ml-0">
           <img src="../assets/images/logo.png" alt="Logo" class="h-12 xl:h-auto">      
         </NuxtLink>
-        <div class="absolute left-0 flex flex-col xl:flex-row items-center w-full text-2xl text-center top-full xl:relative bg-darkgray">
+        <div class="absolute left-0 flex flex-col xl:flex-row items-center w-full text-2xl text-center top-full xl:relative bg-section z-20" :class="{'-translate-x-full':!menuOpen}">
           <ul class="xl:flex pt-10 pb-8">
             <li class="mb-5 xl:mb-0 xl:mr-5">
               <NuxtLink to="/" class=" navlink text-light active">
@@ -51,10 +53,6 @@
             </li>
           </ul>
           
-          
-          
-         
-          
           <div class="w-full xl:w-[364px] h-[44px] xl:rounded-full border border-secondary ml-auto xl:mr-6 xl:px-5 flex xl:hidden 2xl:flex justify-between items-center order-first xl:order-none">
             <input type="text" class="bg-transparent shadow-none border-none outline-none h-[44px] text-2xl w-full">
             <button type="button" class="duration-150 hover:translate-x-1">
@@ -72,30 +70,22 @@
               <span>1</span>
             </div>
           </button>
-          <button class="block h-[42px] xl:rounded-[20px/21px] py-[6px] xl:px-2 xl:border-l-4 xl:border-r-4 xl:border-secondary w-full xl:w-auto">
+          <button class="block h-[42px] xl:rounded-[20px/21px] py-[6px] xl:px-2 xl:border-l-4 xl:border-r-4 xl:border-secondary w-full xl:w-auto bg-primary">
             <div class="h-[32px] flex justify-center items-center xl:rounded-[14px/16px] xl:px-4 xl:border-l-4 xl:border-r-4 xl:border-secondary">
               →
               <span class="ml-2 text-xl text-secondary whitespace-nowrap">登入</span>
             </div>
           </button>
-
-          
         </div>
-        
-        
       </div>
     </div>
-    
-
-
+  
   </div>
 
 </template>
 
 <style lang="scss" scoped>
-*{
-  // outline: 1px solid #F00
-}
+
 .navlink{
   position: relative;
   display: inline-block;
