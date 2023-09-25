c14ae21894530033e6e68c33e27977c178f8a241
yichienlu <yichien.lu89@gmail.com>
2023-09-25 17:14:46 +0800

    feat: nav item hover effect

--- components/NavbarYC.vue
+++ components/NavbarYC.vue
@@ -3,6 +3,8 @@
 // const toggleMenu = () => {
 //   menuOpen.value = !menuOpen.value
 // }
+
+
 </script>
 
 <template>
@@ -14,24 +16,46 @@
         </svg>
       </button>
       <div class="container z-20 flex items-center justify-between py-2 mx-auto xl:py-6 ">
-        <NuxtLink to="/" class="w-auto mx-auto xl:mr-8 xl:ml-0 bg-secondary">
+        <NuxtLink to="/" class="w-auto mx-auto xl:mr-8 xl:ml-0">
           <img src="../assets/images/logo.png" alt="Logo" class="h-12 xl:h-auto">      
         </NuxtLink>
         <div class="absolute left-0 flex flex-col xl:flex-row items-center w-full text-2xl text-center top-full xl:relative bg-darkgray">
-          <NuxtLink to="/" class="block mr-5 navlink text-maintext hover:text-secondary">
-            <div>遊戲商店</div>
-            </NuxtLink>
-          <NuxtLink to="/" class="block mr-5 navlink text-maintext hover:text-secondary">
-            <div>遊戲類別</div>
-          </NuxtLink>
-          <NuxtLink to="/" class="block mr-5 navlink text-maintext hover:text-secondary">
-            <div>遊戲新聞</div>
-          </NuxtLink>
-          <NuxtLink to="/" class="block mr-5 navlink text-maintext hover:text-secondary">
-            <div>收藏庫</div>
-          </NuxtLink>
+          <ul class="xl:flex pt-10 pb-8">
+            <li class="mb-5 xl:mb-0 xl:mr-5">
+              <NuxtLink to="/" class=" navlink text-light active">
+                <span class="pseudo">遊戲商店</span>
+                <span data-text="遊戲商店" class="part"></span>
+                <span data-text="遊戲商店" class="part"></span>
+              </NuxtLink>
+            </li>
+            <li class="mb-5 xl:mb-0 xl:mr-5">
+              <NuxtLink to="/" class="block navlink  text-light">
+                <span class="pseudo">遊戲類別</span>
+                <span data-text="遊戲類別" class="part"></span>
+                <span data-text="遊戲類別" class="part"></span>
+              </NuxtLink>
+            </li>
+            <li class="mb-5 xl:mb-0 xl:mr-5">
+              <NuxtLink to="/" class="block navlink  text-light">
+                <span class="pseudo">遊戲新聞</span>
+                <span data-text="遊戲新聞" class="part"></span>
+                <span data-text="遊戲新聞" class="part"></span>
+              </NuxtLink>
+            </li>
+            <li class="mb-5 xl:mb-0 xl:mr-5">
+              <NuxtLink to="/" class="block navlink  text-light">
+                <span class="pseudo">收藏庫</span>
+                <span data-text="收藏庫" class="part"></span>
+                <span data-text="收藏庫" class="part"></span>
+              </NuxtLink>
+            </li>
+          </ul>
           
-          <div class="xl:w-[364px] h-[44px] xl:rounded-full border border-secondary ml-auto xl:mr-6 xl:px-5 flex xl:hidden 2xl:flex justify-between items-center order-first xl:order-none">
+          
+          
+         
+          
+          <div class="w-full xl:w-[364px] h-[44px] xl:rounded-full border border-secondary ml-auto xl:mr-6 xl:px-5 flex xl:hidden 2xl:flex justify-between items-center order-first xl:order-none">
             <input type="text" class="bg-transparent shadow-none border-none outline-none h-[44px] text-2xl w-full">
             <button type="button" class="duration-150 hover:translate-x-1">
               →
@@ -70,26 +94,90 @@
 
 <style lang="scss" scoped>
 *{
-  outline: 1px solid #F00
+  // outline: 1px solid #F00
 }
 .navlink{
   position: relative;
-  padding-left: 10px;
-  padding-right: 10px;
+  display: inline-block;
+  padding-left: 20px;
+  padding-right: 20px;
+
+  transition: 0.2s;
+
+  &.active{
+    color: rgb(16 216 159 / var(--tw-text-opacity));
+
+    &::before {
+    background-color: rgb(16 216 159 / var(--tw-text-opacity));
+
+    }
+  }
+
   &::before{
     content:'';
+    display: block;
     position: absolute;
-    top: 50%;
-    left:0;
-    background-color: #10D89F;
+    background-color: #c8c8c8;
     height: 1px;
-    width: 0%;
-    transition: .3s ease-in-out;
+    z-index: 50;
+    top: 50%;
+    right: 0;
+    width: 0;
+
+    transition: width 0.2s;
   }
-  &:hover::before {
+  &:hover
+  {
+    &::before {
     width: 100%;
+    left:0;
+    transition: width position 0.2s
+  }
   }
 }
 
 
+.navlink .pseudo {
+  padding-left: 2px;
+  padding-right: 2px;
+    opacity: 0;
+    // background-color: #FAA5;
+}
+
+.navlink .part {
+    width: 100%;
+    position: absolute;
+    height: 50%;
+    overflow: hidden;
+    top: 0;
+    left: 20px;
+    // transition-duration: .3s;
+
+}
+
+.navlink .part:after {
+    content: attr(data-text);
+    position: absolute;
+
+    top: 0;
+    left: 0px;
+}
+
+.navlink .part + .part {
+    top: 50%;
+}
+
+.navlink .part + .part:after {
+    top: -100%;
+    transition: .3s;
+}
+
+
+.navlink:hover .part + .part:after {
+    left: 4px;
+}
+.navlink:hover {
+  transform: translateX(-2px)
+
+}
 </style>
