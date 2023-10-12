bc194f8f0ab06d634ce4066da493b811774172ba
Peter <guanwei0702@gmail.com>
2023-10-12 17:06:58 +0800

    feat: Footer樣式tailwind改寫

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,32 +3,25 @@
    
 </script>
 <template>
-   <footer class="footer pt-8">
-      <div class="container py-4">
-        <span class="lg:mr-8">COPYRIGHT © 2023</span>
-        <span>此為多人協作練習作品，非營利用途</span>
+   <footer class="footer w-2/4 m-auto pt-8">
+      <div class="container bg-section py-4 text-center">
+        <span class="block lg:inline lg:mr-8 decoration-light">COPYRIGHT © 2023</span>
+        <span class="block lg:inline">此為多人協作練習作品，非營利用途</span>
       </div>
    </footer>
 </template>
 <style>
   .footer{
-    width: 50%;
-    margin: auto;
+    /* width: 50%;
+    margin: auto; */
   }
   .container{
     border-radius: 40px 40px 0px 0px;
-    background: #303141;
-    text-align: center;
+    /* background: #303141; */
+    /* text-align: center; */
   }
   .container span{
-    display: block;
     font-size: clamp(14px, 2.5vw, 18px);
-    color: #D9D9D9;
-  }
-  @media screen and (min-width:1024px){
-    .container span{
-      display: inline;
-    }
-    
+    /* color: #D9D9D9; */
   }
 </style>
