8f5b98580c467bc8b65b3c8ea98ba33b97f22ddb
yichienlu <yichien.lu89@gmail.com>
2023-10-20 11:42:57 +0800

    fix: use local data

--- components/game/SectionTwoComponent.vue
+++ components/game/SectionTwoComponent.vue
@@ -1,22 +1,12 @@
 <script setup>
-import db from "@/db.json"
-const item = db.game[0]
+import db from "@/db1.json"
 
+const data = ref(db)
 
-// const data = item[item.id].data
-
-
-const data = ref({})
-const getData = async () => {
-  await useFetch('https://store.steampowered.com/api/appdetails?appids=1446780&l=tchinese').then((res)=>{
-  data.value = res.data._rawValue[1446780].data
-  console.log(data.value)
-})
-}
-getData()
 
 </script>
 <template>
+
   <section class="py-10 lg:py-14 bg-section text-light">
     <div class="container mx-auto">
       <div class="grid gap-x-14 lg:grid-cols-3">
@@ -29,24 +19,24 @@ getData()
                   <span class="font-medium text-24">description</span>
                 </h2>
                 <ul id="tags" class="flex flex-wrap text-[12px] text-white">
-                  <li v-for="tagItem in data.categories" class="px-3 py-1 mb-2 mr-2 rounded bg-tab" :key="tagItem">{{ tagItem.description }}</li>
-                  <li v-for="tagItem in data.genres" class="px-3 py-1 mb-2 mr-2 rounded bg-tab" :key="tagItem">{{ tagItem.description }}</li>
+                  <li v-for="tagItem in data?.categories" class="px-3 py-1 mb-2 mr-2 rounded bg-tab" :key="tagItem">{{ tagItem.description }}</li>
+                  <li v-for="tagItem in data?.genres" class="px-3 py-1 mb-2 mr-2 rounded bg-tab" :key="tagItem">{{ tagItem.description }}</li>
                 </ul>
               </div>
               <div v-if="data?.metacritic?.score" class="flex items-center justify-center order-3 w-16 h-16 bg-score-high lg:order-2 rounded-xl text-28 lg:text-40 lg:rounded-3xl lg:w-20 lg:h-20" :class="{'bg-score-high':data?.metacritic?.score>=80, 'bg-score-mid':data?.metacritic?.score<80 && data?.metacritic?.score>=40, 'bg-score-low':data?.metacritic?.score<40}">
                 {{data?.metacritic?.score / 10}}
               </div>
                 <ul class="order-2 lg:order-3 ">
-                  <li>發行日期：{{ data.release_date.date }} </li>
-                  <li>開發人員：{{data.developers.join("、")}}</li>
-                  <li>發行商：{{data.publishers.join("、")}}</li>
+                  <li>發行日期：{{ data?.release_date.date }} </li>
+                  <li>開發人員：{{data?.developers.join("、")}}</li>
+                  <li>發行商：{{data?.publishers.join("、")}}</li>
                 </ul>
                 <button class="order-4 w-full button-outline-md button-outline-hover">評論此遊戲</button>
               </div>
-              <p v-html="data.about_the_game"></p>
+              <p v-html="data?.about_the_game"></p>
           </div>
           
-          <div id="combo" class="lg:row-span-2 " v-if="data.packages">
+          <div id="combo" class="lg:row-span-2 " v-if="data?.packages">
             <h2 class="pb-1 mb-5 border-b border-secondary lg:border-transparent relative 
             after:absolute 
             after:w-full 
@@ -59,7 +49,7 @@ getData()
               <span class="font-medium text-24">Special</span>
             </h2>
             <ul>
-              <li v-for="packageItem in data.package_groups[0].subs.slice(1)" class="px-4 pt-6 pb-5 mb-7 border border-labellight  rounded-[20px]" :key="packageItem.packageid">
+              <li v-for="packageItem in data?.package_groups[0].subs.slice(1)" class="px-4 pt-6 pb-5 mb-7 border border-labellight  rounded-[20px]" :key="packageItem.packageid">
                 <h3 class="mb-2 text-24">購買豪華禮包</h3>
                 <p class="mb-4 text-18">{{packageItem.option_text.split(' - NT$')[0]}}</p>
                 <div class="flex items-center mb-6">
@@ -96,11 +86,9 @@ getData()
               <li class="mb-10">
                 <div class="items-center md:flex">
                   <div class="h-[152px] md:h-[200px] md:w-1/2 md:mr-14 bg-red-400">圖</div>
-                  <!-- <img src="" class="h-[152px] md:h-[200px]" alt=""> -->
                   <div class="md:w-1/2 ">
                     <h3 class="mt-8 mb-2 text-20 md:mt-0">A CRAZY JOURNEY - 10 MILLION UNITS SOLD!</h3>
                     <p class="ellipsis-3 ">It’s so amazing to see all the support for It Takes Two. Thank you to all the fans and community for going on this magical journey together with us!-Hazelight Studios
-                      <!-- <span>more</span> -->
                     </p>
                   </div>
                 </div>
@@ -109,7 +97,6 @@ getData()
               <li class="mb-10">
                 <div class="items-center md:flex">
                   <div class="h-[152px] md:h-[200px] md:w-1/2 md:mr-14 bg-red-400">圖</div>
-                  <!-- <img src="" class="h-[152px] md:h-[200px]" alt=""> -->
                   <div class="md:w-1/2 ">
                     <h3 class="mt-8 mb-2 text-20 md:mt-0">A CRAZY JOURNEY - 10 MILLION UNITS SOLD!</h3>
                     <p class="ellipsis-3">It’s so amazing to see all the support for It Takes Two. Thank you to all the fans and community for going on this magical journey together with us!-Hazelight Studios</p>
