372c70e9e4bf9231d7971ac0cd859493f3a23c42
yichienlu <yichien.lu89@gmail.com>
2023-11-21 10:32:43 +0800

    fix: navbar category menu height solved

--- components/NavbarComponent/index.vue
+++ components/NavbarComponent/index.vue
@@ -136,8 +136,7 @@ const checkScrollbar = (e) =>{
           <!-- 手機版 -->
           <div class="flex xl:hidden duration-300 w-[300%]" >
             <div class="duration-300" :class="{'w-[33.33%]':!categoryOpen, 'w-0':categoryOpen}"></div>
-            <ul class="xl:hidden pt-10 pb-8  w-[33.33%] duration-300 "
-            >
+            <ul class="xl:hidden pt-10 pb-8  w-[33.33%] duration-300 ">
               <li class="mb-5">
                 <NuxtLink to="/game/1" class="block text-light navlink " @click="closeMenu">
                   <div class="nav-design">
@@ -181,7 +180,7 @@ const checkScrollbar = (e) =>{
             </ul>
 
             <!-- 類別清單 -->
-            <ul ref="categoryMenu" class="pt-10 pb-8 w-[33.33%] overflow-y-scroll overscroll-contain duration-300 category-scrollbar relative" :class="{'h-0':!categoryOpen, 'h-[calc(100vh-132px)]':categoryOpen}"
+            <ul ref="categoryMenu" class="pt-10 pb-8 w-[33.33%] overflow-y-scroll overscroll-contain duration-300 category-scrollbar relative" :class="{'h-0':!categoryOpen, 'h-[calc(100vh-174px)]':categoryOpen && !loginStore.loggedIn, 'h-[calc(100vh-108px)]':categoryOpen && loginStore.loggedIn}"
             @wheel.stop="checkScrollbar"
             @touchmove.stop="checkScrollbar"
             @scroll.stop="checkScrollbar"
@@ -265,14 +264,14 @@ const checkScrollbar = (e) =>{
           </div>
 
           <!-- 搜尋欄 -->
-          <div v-if="!isHome" class="w-full xl:max-w-[364px] h-[44px] xl:rounded-full border-y xl:border border-secondary ml-auto xl:mr-6 pr-5 xl:pl-5 flex 2xl:flex justify-between items-center order-first xl:order-none">
+          <div class="w-full xl:max-w-[364px] h-[44px] xl:rounded-full border-y xl:border border-secondary ml-auto xl:mr-6 pr-5 xl:pl-5 flex 2xl:flex justify-between items-center order-first xl:order-none" :class="{'xl:none':!isHome}">
             <input type="text" class="bg-transparent shadow-none border-none outline-none h-[44px] text-2xl w-full">
             <button type="button" class="duration-150 text-white hover:text-secondary flex items-center">
               <Icon name="uil:search" />
             </button>
           </div>
           <!-- 搜尋按鈕 -->
-          <button v-else class="w-[50px] h-[50px] rounded-full border ml-auto mr-4 hidden xl:flex justify-center items-center text-white hover:text-secondary hover:border-secondary">
+          <button v-if="!isHome" class="w-[50px] h-[50px] rounded-full border ml-auto mr-4 hidden xl:flex justify-center items-center text-white hover:text-secondary hover:border-secondary">
             <Icon name="uil:search" />
           </button>
           <!-- 購物車 -->
@@ -290,11 +289,11 @@ const checkScrollbar = (e) =>{
 
         <!-- 會員中心 -->
         <div class="absolute top-0 right-0 xl:relative w-[122px] h-16 xl:h-[92px] flex flex-col items-center justify-center group " :class="{'hidden':!loginStore.loggedIn}">
-          <a href="#" class="absolute top-0 flex flex-col items-center justify-center w-full h-full xl:bg-primary z-40">
+          <a href="#" class="absolute top-0 flex flex-col items-center justify-center w-full h-full bg-section xl:bg-primary z-40">
             <img src="https://s3-alpha-sig.figma.com/img/cfbf/8e85/c101d9ee509ebd1615efb93f1332b8d2?Expires=1701648000&Signature=BS6nVO7d6q6c03Ihi0h2zyaSAbTnNJMWK1y2Q5axpUmNrJO4d~p0eSFMcSm7snivW2lwcCmBPBQIqDLDK1BZeZ7awwmLanq78qjyMG~VzKoF11Wq8TcWPx~8yudJli6heuM8ShKKLTA-QB1BoH0aJEwofJSuq4LRKDSJXGa-BXQEZ5JnJEuta9sLrU20wjARSMjC8JJxeLhnfyrTs1pA9zwPVjSaTIgsEJ~W7NGB1Ovu9l3K9QiVui3qjDRq-6KA14usvOhMD02Z-XiN9QkKBnOih9gZWE5Q7XWxahlJEu2GbMv6MjczvbdZxaqBOvfocahyG-Fsd5zoB~-gao2org__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" class="block w-[52px] h-[52px] rounded-xl object-cover">
             <div class="text-base">Mouse3153</div>
           </a>
-          <div class="absolute top-full w-full  -translate-y-full group-hover:-translate-y-0 transition-transform duration-300">
+          <div class="absolute top-full w-full  -translate-y-full group-hover:-translate-y-0 transition-transform duration-300 z-30">
             <ul class="rounded-b-xl shadow border border-primary text-center ">
               <li class="hover:text-secondary bg-section border border-primary">
                 <NuxtLink to="/" class="block py-2">收藏庫</NuxtLink>
@@ -320,7 +319,4 @@ const checkScrollbar = (e) =>{
 
 <style lang="scss" scoped>
   @import url("./style.css");
-  *{
-    outline: 1px solid #A00
-  }
 </style>
