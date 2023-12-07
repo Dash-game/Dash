1cee0405b8b94ab376e29e648ccf16c5f7f5e29f
yichienlu <yichien.lu89@gmail.com>
2023-12-07 11:34:47 +0800

    fix: validate rule

--- pages/login.vue
+++ pages/login.vue
@@ -39,9 +39,7 @@
                 <VeeErrorMessage name="email" class="text-sm text-labeldark mt-1 absolute top-full left-1" />
               </div>
               <div class="mb-16 relative">
-                <!-- ↓測試暫用 -->
-                <VeeField name="password" type="password" class="w-full h-16 bg-transparent text-2xl font-medium border border-light rounded-xl focus:border-light focus:ring-light" placeholder="Password" rules="required" />
-                <!-- <VeeField name="password" type="password" class="w-full h-16 bg-transparent text-2xl font-medium border border-light rounded-xl focus:border-light focus:ring-light" placeholder="Password" rules="required|min:8|uppercase" /> -->
+                <VeeField name="password" type="password" class="w-full h-16 bg-transparent text-2xl font-medium border border-light rounded-xl focus:border-light focus:ring-light" placeholder="Password" rules="required|min:8|uppercase" />
                 <VeeErrorMessage name="password" class="text-sm text-labeldark mt-1 absolute top-full left-1" />
               </div>
               <button type="submit" class="w-full h-16 rounded-xl text-2xl bg-secondary text-primary hover:text-white">登入</button>
