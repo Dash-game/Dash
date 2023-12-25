6c442e43594d088dd88d029406f169c4b1405da3
lingling <zzxcv741@hotmail.com>
2023-12-25 22:22:11 +0800

    feat: time-format match any format style

--- plugins/timeformat.js
+++ plugins/timeformat.js
@@ -1,17 +1,28 @@
-import dayjs from "dayjs"
+import dayjs from 'dayjs'
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.directive("dateformat", {
-    mounted(el, binding) {
-      const date = dayjs(binding.value).format("YYYY-MM-DD");
-      el.innerText = date;
-    },
-  }).directive("timeformat", {
-    mounted(el, binding) {
-      const time = dayjs(binding.value).format("YYYY-MM-DD HH:mm");
-      el.innerText = time;
-    },
-  })
-  
-  ;
- });
- 
+  nuxtApp.vueApp
+    .directive('dateformat', {
+      mounted(el, binding) {
+        let date
+        if (typeof binding.value === 'object') {
+          const { value, format } = binding.value
+          date = dayjs(value).format(format)
+        } else {
+          date = dayjs(binding.value).format('YYYY-MM-DD')
+        }
+        el.innerText = date
+      }
+    })
+    .directive('timeformat', {
+      mounted(el, binding) {
+        let date
+        if (typeof binding.value === 'object') {
+          const { timestamp, format } = binding.value
+          date = dayjs(timestamp).format(format)
+        } else {
+          date = dayjs(binding.value).format('YYYY-MM-DD HH:mm')
+        }
+        el.innerText = date
+      }
+    })
+})
