924e4f5dfdd326a4ac195026534ac15537bd029f
lingling <zzxcv741@hotmail.com>
2024-01-02 20:55:14 +0800

    fix: filter syntax match -> includes , avoid regex error

--- components/member/listComponent.vue
+++ components/member/listComponent.vue
@@ -4,8 +4,14 @@
       class="flex flex-row justify-between items-center flex-wrap space-y-10 lg:space-y-0 mb-[110px]"
     >
       <!-- search -->
-      <memberSearch v-model="searchValue" @update:searchValue="updateSearchValue"></memberSearch>
-      <memberDropDown :dropData="dropData" @dropdown-sort="dropdownSort" ></memberDropDown>
+      <memberSearch
+        v-model="searchValue"
+        @update:searchValue="updateSearchValue"
+      ></memberSearch>
+      <memberDropDown
+        :dropData="dropData"
+        @dropdown-sort="dropdownSort"
+      ></memberDropDown>
     </div>
     <!-- item -->
     <ul class="grid gap-14">
@@ -113,106 +119,109 @@
   </commonModal>
 </template>
 <script setup>
-  const commentModalData = ref({})
-  const commentModal = ref(null)
+const commentModalData = ref({})
+const commentModal = ref(null)
 
-  const showModal = (data) => {
-    commentModalData.value = { ...data }
-    commentModal.value.show()
+const showModal = (data) => {
+  commentModalData.value = { ...data }
+  commentModal.value.show()
+}
+
+const modalbody = ref(null)
+const sendComment = () => {
+  const commentValue = modalbody.value.getValue()
+  console.log(commentValue)
+}
+
+const dropData = ref([
+  { item: '依收藏日期排序', code: 'date' },
+  { item: '篩選二', code: '' },
+  { item: '篩選三', code: '' }
+])
+const listData = ref([
+  {
+    image: '/images/member/memberGameImage001.png',
+    title: 'Dave the Diver',
+    publishers: ['Frictional Games'],
+    release_date: 1702736324,
+    last_play_time: 1901000000,
+    playTime: 1000,
+    is_recommend: 0 // -1 = 不推薦,0 = 尚未評論,1 = 推薦
+  },
+  {
+    image: '/images/member/memberGameImage001.png',
+    title:
+      'abc the DiverDave the DiverDave the DiverDarDave the DiverDave the DiverDave the DiverDave theerDave the DiverDrDavet heDiverDave the DiverDave the DiverDavethe DiverDave thverDave the Diver',
+    publishers: ['Frictional Games'],
+    release_date: 1702736324,
+    last_play_time: 1501000000,
+    playTime: 2000,
+    is_recommend: 1 // -1 = 不推薦,0 = 尚未評論,1 = 推薦
+  },
+  {
+    image: '/images/member/memberGameImage001.png',
+    title: 'cat the Diver',
+    publishers: ['Frictional Games'],
+    release_date: 1702736324,
+    last_play_time: 2001000000,
+    playTime: 3000,
+    is_recommend: -1 // -1 = 不推薦,0 = 尚未評論,1 = 推薦
+  },
+  {
+    image: '/images/member/memberGameImage001.png',
+    title: 'pink the Diver',
+    publishers: ['Frictional Games'],
+    release_date: 1702736324,
+    last_play_time: 1701000000,
+    playTime: 4000,
+    is_recommend: 0 // -1 = 不推薦,0 = 尚未評論,1 = 推薦
+  },
+  {
+    image: '/images/member/memberGameImage001.png',
+    title: 'Dave the Diver',
+    publishers: ['Frictional Games'],
+    release_date: 1802736324,
+    last_play_time: 1701110000,
+    playTime: 5000,
+    is_recommend: 1 // -1 = 不推薦,0 = 尚未評論,1 = 推薦
   }
-
-  const modalbody = ref(null)
-  const sendComment = () => {
-    const commentValue = modalbody.value.getValue()
-    console.log(commentValue)
-  }
-
-  const dropData = ref([
-    { item: '依收藏日期排序', code: 'date' },
-    { item: '篩選二', code: '' },
-    { item: '篩選三', code: '' }
-  ]);
-  const listData = ref([
-    {
-      image: '/images/member/memberGameImage001.png',
-      title: 'Dave the Diver',
-      publishers: ['Frictional Games'],
-      release_date: 1702736324,
-      last_play_time: 1901000000,
-      playTime: 1000,
-      is_recommend: 0 // -1 = 不推薦,0 = 尚未評論,1 = 推薦
-    },
-    {
-      image: '/images/member/memberGameImage001.png',
-      title:
-        'abc the DiverDave the DiverDave the DiverDarDave the DiverDave the DiverDave the DiverDave theerDave the DiverDrDavet heDiverDave the DiverDave the DiverDavethe DiverDave thverDave the Diver',
-      publishers: ['Frictional Games'],
-      release_date: 1702736324,
-      last_play_time: 1501000000,
-      playTime: 2000,
-      is_recommend: 1 // -1 = 不推薦,0 = 尚未評論,1 = 推薦
-    },
-    {
-      image: '/images/member/memberGameImage001.png',
-      title: 'cat the Diver',
-      publishers: ['Frictional Games'],
-      release_date: 1702736324,
-      last_play_time: 2001000000,
-      playTime: 3000,
-      is_recommend: -1 // -1 = 不推薦,0 = 尚未評論,1 = 推薦
-    },
-    {
-      image: '/images/member/memberGameImage001.png',
-      title: 'pink the Diver',
-      publishers: ['Frictional Games'],
-      release_date: 1702736324,
-      last_play_time: 1701000000,
-      playTime: 4000,
-      is_recommend: 0 // -1 = 不推薦,0 = 尚未評論,1 = 推薦
-    },
-    {
-      image: '/images/member/memberGameImage001.png',
-      title: 'Dave the Diver',
-      publishers: ['Frictional Games'],
-      release_date: 1802736324,
-      last_play_time: 1701110000,
-      playTime: 5000,
-      is_recommend: 1 // -1 = 不推薦,0 = 尚未評論,1 = 推薦
+])
+/**
+ * selected dropdown value
+ * @param { string } selected
+ */
+const selectedSort = ref(null)
+const dropdownSort = (selected) => {
+  selectedSort.value = selected
+  // newListData.value
+}
+/**
+ * feature search
+ */
+const searchValue = ref('')
+const updateSearchValue = (value) => (searchValue.value = value)
+/**
+ * Vue computed
+ */
+const newListData = computed(() => {
+  let result = [...listData.value]
+  // feature sort
+  // console.log(selectedSort.value)
+  if (selectedSort.value) {
+    switch (selectedSort.value.code) {
+      case 'date':
+        result = result.sort((x, y) => y.last_play_time - x.last_play_time)
+        break
+      default:
+        break
     }
-  ]);
-  /**
-   * selected dropdown value
-   * @param { string } selected
-   */
-  const selectedSort = ref(null);
-  const dropdownSort = (selected) => {
-    selectedSort.value = selected;
-    newListData.value;
   }
-  /**
-   * feature search
-   */
-  const searchValue = ref('');
-  const updateSearchValue = value => searchValue.value = value;
-  /**
-   * Vue computed
-   */
-  const newListData = computed(() => {
-    let result = [...listData.value];
-    // feature sort
-    if(selectedSort.value) {
-      switch (selectedSort.value.code) {
-        case 'date':
-          result = result.sort((x, y) => y.last_play_time - x.last_play_time);
-          break;
-        default:
-          break;
-      }
-    }
-    // feature search
-    result = result.filter(item => item.title.toLowerCase().match(searchValue.value.toLowerCase()));
+  // feature search
+  result = result.filter((item) =>
+    item.title.toLowerCase().includes(searchValue.value.toLowerCase())
+  )
 
-    console.log('result:', result);
-    return result;
-  })
+  // console.log('result:', result)
+  return result
+})
 </script>
