d6caa0a156601ebea3b6d823d36b4053338504c1
yichienlu <yichien.lu89@gmail.com>
2024-02-29 22:18:36 +0800

    feat: localStorage test

--- pages/help.vue
+++ pages/help.vue
@@ -1,4 +1,28 @@
+<script setup>
+  const cartList = ref([])
+  const getItem = () => {
+    if(process.client){
+      cartList.value = JSON.parse(localStorage.getItem("dash"))
+    }
+  }
+  const addItem = (id)=>{
+    if(process.client){
+      if(!cartList.value){
+        localStorage.setItem("dash", `${[id]}`)
+        cartList.value = JSON.parse(localStorage.getItem("dash"))
+      }else{
+        cartList.value.push(id)
+        localStorage.setItem("dash",JSON.stringify(cartList.value))
+      }
+    }
+  }
+  getItem()
+</script>
 <template>
-help
-  
+  <div class="h-80">
+    help
+    <button class="bg-red-300" @click="addItem('A123')">add</button>
+    {{ cartList }}
+  </div>
+
 </template>
