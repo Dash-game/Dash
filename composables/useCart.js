e770a435446fef4baa230e08698d6868d7581471
yichienlu <yichien.lu89@gmail.com>
2024-03-15 17:32:30 +0800

    feat: cartStore

--- composables/useCart.js
+++ composables/useCart.js
@@ -1,30 +1,32 @@
 export default function() {
-  const cartList = ref([])
+  const cartStore = useCartStore()
+
+  // const cartList = ref([])
   const getItem = () => {
     if(process.client){
       if(!localStorage.getItem("dash")){
         localStorage.setItem("dash",`{"list":[]}`)
       } else {
-        cartList.value = JSON.parse(localStorage.getItem("dash"))?.list
+        cartStore.cart = JSON.parse(localStorage.getItem("dash"))?.list
       }
     }
   }
   const setItem = (add,id)=>{
     if(process.client){
       if(add){
-        if(cartList.value.length==0){
+        if(cartStore.cart.length==0){
           localStorage.setItem("dash", `{"list":["${id}"]}`)
-          cartList.value = JSON.parse(localStorage.getItem("dash")).list
-        }else if(cartList.value.findIndex((item)=>item==id)==-1){
-          cartList.value.push(`${id}`)
-          localStorage.setItem("dash",`{"list":${JSON.stringify(cartList.value)}}`)
+          cartStore.cart = JSON.parse(localStorage.getItem("dash")).list
+        }else if(cartStore.cart.findIndex((item)=>item==id)==-1){
+          cartStore.cart.push(`${id}`)
+          localStorage.setItem("dash",`{"list":${JSON.stringify(cartStore.cart)}}`)
         }
       } 
       else {
-        const index = cartList.value.findIndex((item)=>item==id)
+        const index = cartStore.cart.findIndex((item)=>item==id)
         if(index!==-1){
-          cartList.value.splice(index,1)
-          localStorage.setItem("dash",`{"list":${JSON.stringify(cartList.value)}}`)
+          cartStore.cart.splice(index,1)
+          localStorage.setItem("dash",`{"list":${JSON.stringify(cartStore.cart)}}`)
         }
       }
     }
@@ -32,7 +34,7 @@ export default function() {
   getItem()
 
   return {
-    cartList,
+    // cartList,
     setItem
   };
 
--- pages/help.vue
+++ pages/help.vue
@@ -1,10 +1,12 @@
 <script setup>
   const { cartList,setItem }=useCart()
+  const cartStore = useCartStore()
 
+  const cart = computed(()=>cartStore.cart)
 </script>
 <template>
   <div class="h-80">
-    {{ cartList }}
+    {{ cart }}
 
   <div>
     <button class="bg-red-600 p-2 m-2" @click="setItem(true, 'A1')">A1</button>
