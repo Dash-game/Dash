da7e1825ea58b7a7bca17fc0394bf4df66bee94b
susu3131 <anny81327@gmail.com>
2024-03-27 13:18:42 +0800

    feat : 新增頁面

--- pages/cart/checkout-finish.vue
+++ pages/cart/checkout-finish.vue
@@ -1,8 +1,28 @@
-<script setup>
-
-</script>
+<script setup></script>
 <template>
-  <div>
-    finish
+  <div class="container ">
+    <div class="py-6 border-y">
+      <p class="mb-3 font-bold text-32 text-labellight">感謝您的購買！</p>
+      <p class="mb-1 text-20">收據已發送至您的電子信箱</p>
+      <p class=" text-20">您現在可立即安裝遊戲或稍後至收藏庫進行下載</p>
+    </div>
+
+    <!-- content -->
+    <div class="py-6">
+      <ul>
+        <li class="flex items-end justify-between "> 
+          <div class="flex "> 
+            <img src="https://fakeimg.pl/350x200" class=" rounded-2xl">
+            <div class=" pl-7">
+              <h2 class="font-medium text-28">PUBG: BATTLEGROUNDS</h2>
+              <p class=" text-18"><span>預購中</span>，您可先進行安裝並於2024-02-10進行遊戲</p>
+            </div>
+
+          </div>
+          <button class=" button-md w-[264px] button-hover">安裝</button>
+        </li>
+      </ul>
+      
+    </div>
   </div>
 </template>
